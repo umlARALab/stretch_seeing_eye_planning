cb4c5980855fa49a4cf8ffc3c29678e15b511019
SkiHatDuckie <SkiHatDuckie@gmail.com>
2024-08-08 14:52:28 -0400

    Implement parameters for filter

--- launch/planning.launch.py
+++ launch/planning.launch.py
@@ -1,13 +1,32 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    params_file_set = PythonExpression(
+        ["'", LaunchConfiguration("param_file"), "' != ''"]
+    )
     return LaunchDescription([
+        DeclareLaunchArgument(
+            "param_file",
+            default_value="",
+            description="A parameters .yaml file for object filter/planning configuration."
+        ),
         Node(
             package="stretch_seeing_eye_planning",
             executable="object_filter",
-            output="screen"
+            output="screen",
+            parameters=[LaunchConfiguration("param_file")],
+            condition=IfCondition(params_file_set)
+        ),
+        Node(
+            package="stretch_seeing_eye_planning",
+            executable="object_filter",
+            output="screen",
+            condition=UnlessCondition(params_file_set)
         ),
         Node(
             package="stretch_seeing_eye_planning",
--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -1,13 +1,46 @@
+#include <chrono>
+#include <functional>
 #include <memory>
+#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
+#include "stretch_seeing_eye_msgs/msg/detected_object.hpp"
+
+using std::placeholders::_1;
 
 class ObjectFilter : public rclcpp::Node {
 public:
+    using DetectedObject = stretch_seeing_eye_msgs::msg::DetectedObject;
     ObjectFilter() : Node("object_filter") {
-
+        this->declare_parameter("retain_within_bounding_box", std::vector<long int>{0, 0, 240, 240});
+        this->declare_parameter("minimum_score", 0.0);
+        
+        sub_ = this->create_subscription<DetectedObject>(
+            "object_filter", 10, std::bind(&ObjectFilter::detection_callback, this, _1)
+        );
+        pub_ = this->create_publisher<DetectedObject>("filtered_objects", 10);
     }
 private:
+    rclcpp::Publisher<DetectedObject>::SharedPtr pub_;
+    rclcpp::Subscription<DetectedObject>::SharedPtr sub_;
+
+    bool detection_within_bounding_box(const DetectedObject &msg) const {
+        std::vector<long int> retention_box = this->get_parameter("retain_within_bounding_box")
+            .as_integer_array();
+        return msg.bounding_box_center[0] <= retention_box[0]
+               && msg.bounding_box_center[0] >= retention_box[0] + retention_box[2]
+               && msg.bounding_box_center[1] <= retention_box[1]
+               && msg.bounding_box_center[1] >= retention_box[1] + retention_box[3];
+    }
+
+    void detection_callback(const DetectedObject &msg) const {
+        double minimum_score = this->get_parameter("minimum_score").as_double();
+
+        if (msg.score >= minimum_score
+            && detection_within_bounding_box(msg)) {
+            pub_->publish(msg);
+        }
+    }
 };
 
 int main(int argc, char* argv[]) {
